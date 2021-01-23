af5960e0079e26fd1d7e7f5823379ba2acb64afe
lih <mrlih94@163.com>
2021-01-23 12:20:48 +0800

    20210123:把命令分开传给ssh执行，放弃给ssh传list

--- DirModifyTime.py
+++ DirModifyTime.py
@@ -6,27 +6,23 @@ cmd_dir_modify_time = []
 my_dir = ['/install', '/home']
 for my_dir_tmp in my_dir:
     cmd_dir_modify_time.append('find ' + my_dir_tmp + ' -mtime -3 -printf \'%CY%Cm%Cd,%CH:%CM,%p\n\'')
-# print(cmd_dir_modify_time)
 
-
-res = func.ssh('192.168.3.78', cmd_dir_modify_time)[0].split('\n')
-modify_time = []
-for res_tmp in res:
-    # print(res_tmp.strip())
-    time_string = res_tmp.strip().split(',')
-    # print(time_string[0], time_string[1])
-    # 将时间保存至字符串
-    time_string = time_string[0] + ' ' + time_string[1]
-    # 将时间字符串转换为时间数组
-    time_array = time.strptime(time_string, '%Y%m%d %H:%M')
-    # 将时间数组转换为时间戳
-    time_stamp = int(time.mktime(time_array))
-    modify_time.append(time_stamp)
-# print(modify_time)
-# 转换为时间戳后就可以通过list输出最大值
-# print(max(modify_time))
-# 将list中最大的时间戳转换为时间数组
-time_array2 = time.localtime(max(modify_time))
-# 将时间数组转换为时间字符串
-time_string2 = time.strftime('%Y%m%d %H:%M:%S', time_array2)
-print(time_string2)
+for cmd_dir_modify_time_tmp in cmd_dir_modify_time:
+    res = func.ssh('192.168.3.78', cmd_dir_modify_time_tmp)[0].split('\n')
+    modify_time = []
+    for res_tmp in res:
+        time_string = res_tmp.strip().split(',')
+        # 将时间保存至字符串
+        time_string = time_string[0] + ' ' + time_string[1]
+        # 将时间字符串转换为时间数组
+        time_array = time.strptime(time_string, '%Y%m%d %H:%M')
+        # 将时间数组转换为时间戳
+        time_stamp = int(time.mktime(time_array))
+        modify_time.append(time_stamp)
+    # 转换为时间戳后就可以通过list输出最大值
+    # print(max(modify_time))
+    # 将list中最大的时间戳转换为时间数组
+    time_array2 = time.localtime(max(modify_time))
+    # 将时间数组转换为时间字符串
+    time_string2 = time.strftime('%Y%m%d %H:%M:%S', time_array2)
+    print(cmd_dir_modify_time_tmp.strip().split(' ')[1] + ',' + time_string2)
